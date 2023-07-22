933860df15d5b6dae14917aab418724d5d264096
Arthur LEROUX <arthur.leroux33@protonmail.com>
2023-07-22 20:55:43 +0200

    react to user response

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const readline = require('readline'); // 
+
+// Ajouter ici le ou les require() pour importer les modules dont vous aurez besoin.
+
+// Hit parade, classé du premier au dernier.
+const hitParade = [
+    `Jain - Come`,
+    `Matt Simons - Catch & Realease`,
+    `Twety One Pilots - Stressed Out`,
+    `Justin Bieber - Love Yourself`,
+    `Kids United - On écrit sur les murs`,
+    `Rihanna - Work`,
+    `Julian Perretta - Miracle`,
+    `Yall - Hundred Miles`,
+    `Kendji Girac - No Me Mirès Màs`,
+    `Feder - Blind (feat. Emmi)`
+];
+
+
+// Votre code va ici
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+
+rl.question('Que voulez vous ?', (answer) => {
+        console.log(`Vous voulez ${answer} `);
+        if(answer === 'chante'){
+            console.log(`Je m'appelle Charlu, je m'appelle Lili, vous êtes chez O'clock.`);
+        }
+});
+
+
+
+
+
