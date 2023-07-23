ed4d3fb1363f6884a2c005369e41dca22c449690
Arthur LEROUX <arthur.leroux33@protonmail.com>
2023-07-23 18:33:21 +0200

    finish exo

--- index.js
+++ index.js
@@ -23,32 +23,39 @@ const rl = readline.createInterface({
 });
 
 function question() {
-  rl.question("Que voulez vous ?", (answer) => {
-    console.log(`Vous voulez ${answer} `);
-    if (answer === `chante`) {
-      console.log(
-        `Je m'appelle Charlu, je m'appelle Lili, vous êtes chez O'clock.`
-      );
-      rl.close();
-    } else if (answer === `classement`) {
-      console.log(`Voici les 10 premier du hit parade ${hitParade.join("\n")}`);
-      rl.close();
-    } else if (answer === `quitter`) {
-        rl.close();
-    } else {
-      response();
+      console.log(`Que voulez-vous ? `);  //Je pose la question ?
     }
-  });
-}
+
+
+    rl.on('line',(answer)=>{
+      if (answer === `chante`) {
+        console.log(
+          `Je m'appelle Charlu, je m'appelle Lili, vous êtes chez O'clock.` // si l'utilisateur demande chante !
+        );
+       question();
+      } else if (answer === `classement`) {
+        console.log(`Voici les 10 premier du hit parade ${hitParade.join("\n")}`); // si il souhaite le classement !
+        question();
+      } else if (answer === `quitter`) {
+        rl.close();                                                               // si il souhaite quitter
+        process.exit();
+      } else if (answer.substring(0, 8)=== 'position') {   // si il souhaite la postion précise ! 
+        const index = answer.replace('position ','');   // je replace la position dans un string vide 
+        const song = hitParade[index -1];  //je selectionne un index du hitparade
+        if(song){
+        console.log(`Position  ${index} : ${song}`); // si la position existe !
+        question();
+      }else {
+        console.log(`Pas de chansons`); // si la position n'existe pas 
+        question();  // et je relance la question a chaque étape sauf a quitter !
+      }
+      }else {
+        console.log(`Je n'ai pas compris votre question ?`);  // si aucune des conditions n'est rempli 
+        question();
+      }
+    });
+  
+
 question();
 
-function response(answer) {
-  console.log(`Vous voulez ${answer} `);
-  if (answer !== `chante`) {
-    console.log(`Je n'ai pas compris la question`);
-    question();
-  } else if (answer !== `classement`) {
-    console.log(`Je n'ai pas compris la Question`);
-    question();
-  }
-}
+
